02bf53e2122dfd719ee2f918fc541147f2ee6a2e
lddzsl <1481607270@qq.com>
2014-08-11 15:14:41 +0800

    Create hello.c
    first hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\n============================\n");
+    printf("\*    Hello World!           *");
+    printf("\n============================\n");
+    return 0;
+}
