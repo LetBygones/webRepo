283d8a9edec9503705f1ed7332d2e82cdfdd12c8
lddzsl <1481607270@qq.com>
2014-08-11 15:54:39 +0800

    7th commit:on develop, add a new function--funPrintMessage.7th commit:on

---
+++ funPrintMessage.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+extern funPrintMessage(void);
+int funPrintMessage(void)
+{
+    printf("\n\tI work at keji zhongyi lu #19.\n");
+    return 0;
+
+}
--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+extern int funPrintMessage(void);
 int main(void)
 {
     printf("\n============================\n");
     printf("*    Hello World!          *");
+    funPrintMessage();
     printf("\n============================\n");
     return 0;
 }
